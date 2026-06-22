853647736b06c8c350a743e44fde4359b56dddb5
Josh Rieken <joshua@joshuarieken.com>
2026-06-22 01:14:01 -0500

    fix: import assign from Phoenix.Component in ObanAuthHook

--- lib/rule_maven_web/live/oban_auth_hook.ex
+++ lib/rule_maven_web/live/oban_auth_hook.ex
@@ -1,14 +1,18 @@
 defmodule RuleMavenWeb.ObanAuthHook do
   @moduledoc """
   LiveView on_mount hook for Oban dashboard. Only allows game_master users.
+  Reads user from browser session (set by AuthPlug).
   """
   import Phoenix.LiveView
+  import Phoenix.Component, only: [assign: 2]
 
-  def on_mount(:default, _params, _session, socket) do
-    user = socket.assigns[:current_user]
+  def on_mount(:default, _params, session, socket) do
+    user_id = session["user_id"]
+
+    user = if user_id, do: RuleMaven.Users.get_user(user_id), else: nil
 
     if user && RuleMaven.Users.game_master?(user) do
-      {:cont, socket}
+      {:cont, assign(socket, current_user: user)}
     else
       {:halt, redirect(socket, to: "/")}
     end
